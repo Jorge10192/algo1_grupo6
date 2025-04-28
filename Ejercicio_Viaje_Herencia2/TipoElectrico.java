4bb8a65cf086d9646250f6ad6b14706cb0671957
franconieto3 <88352320+franconieto3@users.noreply.github.com>
2025-04-28 20:32:23 -0300

    Delete Ejercicio_Viaje_Herencia2 directory

--- Ejercicio_Viaje_Herencia2/TipoElectrico.java
+++
@@ -1,8 +0,0 @@
-package viaje_b;
-
-public class TipoElectrico extends TipoDeViaje {
-    @Override
-    public double tiempoDeDemora(Trayecto trayecto, int capacidadPasajeros) {
-        return (trayecto.getDistanciaKm() * trayecto.getCantidadEstaciones()) / 2.0;
-    }
-}

d057aba84f8fbcd3338f85abbcfd0a3d1035a219
Franco <fgnieto@estudiantes.unsam.edu.ar>
2025-06-01 17:35:22 -0300

    agrego archivos

---
+++ Trabajo Practico Final - Algo1/src/exceptions/EncodingException.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+import importador.EncodingUtils;
+
+public class EncodingException extends Exception {
+    private final String encoding;
+
+    public EncodingException(String encoding){
+        super();  // Llama al constructor de Exception (opcional si no hay mensaje)
+        this.encoding = encoding;
+    }
+
+    @Override
+    public String getMessage(){
+        String mensaje = "El encoding utilizado: " + encoding + " no es válido. Utilice alguno de los siguientes: \n";
+        mensaje += EncodingUtils.encodings.toString();
+        return mensaje;
+    }
+}
+

5498472a3e7bbd3a5ee7416b533ef59bea7ed951
Jorge10192 <82921787+Jorge10192@users.noreply.github.com>
2025-06-07 08:30:49 -0300

    Delete Trabajo Practico Final - Algo1 directory

--- Trabajo Practico Final - Algo1/src/exceptions/InvalidTypeException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-public class InvalidTypeException extends RuntimeException{
-    public InvalidTypeException(){
-        super("El tipo de la celda no coincide con el de la columna.");
-    }
-}
